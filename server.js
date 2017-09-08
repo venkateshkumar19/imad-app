ba1bca877d1278ada0fa00eef834b0fa3f1cb89a
venkateshkumar19 <30936959+venkateshkumar19@users.noreply.github.com>
2017-09-08 12:06:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content={
+    title: Article One;
+    heading: 
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
